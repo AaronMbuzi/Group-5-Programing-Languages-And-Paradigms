fdb63d75bab90f234f166e1397a8a295b925a36b
Aaron Mbuzi <86435544+AaronMbuzi@users.noreply.github.com>
2023-03-22 14:17:07 +0200

    Update MathCalculator.java

--- Java Calculator/MathCalculator.java
+++ Java Calculator/MathCalculator.java
@@ -3,7 +3,11 @@ public class MathCalculator {
         //scanner can come here
         //Switch statement goes here(In the switch statement we call the respective methods within the cases)
     }
-    //addition and subtraction method
-    //multiplication, division, and modulus methode goes here
-    //factorial and power(exponential) goes here
+    //addition method
+    //subtraction method
+    //multiplication method 
+    //division method
+    //modulus method
+    //factorial method
+    //power(exponential)
 }
