9e93392b579694e11fb6f2a13c41c22e8c13e373
Aaron Mbuzi <86435544+AaronMbuzi@users.noreply.github.com>
2023-03-22 14:14:01 +0200

    Add files via upload

---
+++ Java Calculator/MathCalculator.java
@@ -0,0 +1,9 @@
+public class MathCalculator {
+    public static void main(String[]args){
+        //scanner can come here
+        //Switch statement goes here(In the switch statement we call the respective methods within the cases)
+    }
+    //addition and subtraction method
+    //multiplication, division, and modulus methode goes here
+    //factorial and power(exponential) goes here
+}
