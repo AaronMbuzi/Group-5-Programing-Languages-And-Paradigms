b799caf04e93c2db7f35e80d24dc6b77c629d5d6
Luwi Kakoma <luwikakoma1@protonmail.com>
2023-03-23 16:49:31 +0000

    Created an addition method for two numbers

--- Java Calculator/MathCalculator.java
+++ Java Calculator/MathCalculator.java
@@ -4,7 +4,13 @@ public class MathCalculator {
         //Switch statement goes here(In the switch statement we call the respective methods within the cases)
     }
     // New comment
-    //addition method
+    //addition method (for two numbers)
+    //naming method
+       private add(double x,double y){
+        double sum = x+y; //declared a variable sum
+        return sum;
+        System.out.println(sum);
+       };
     //subtraction method
     //multiplication method 
     //division method
