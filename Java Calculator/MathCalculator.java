ecc7739cac19983f0e3ad55e1ff430e99c16101f
Luwi Kakoma <luwikakoma1@protonmail.com>
2023-03-23 12:20:51 +0000

    Added a comment

--- Java Calculator/MathCalculator.java
+++ Java Calculator/MathCalculator.java
@@ -3,6 +3,7 @@ public class MathCalculator {
         //scanner can come here
         //Switch statement goes here(In the switch statement we call the respective methods within the cases)
     }
+    // New comment
     //addition method
     //subtraction method
     //multiplication method 
